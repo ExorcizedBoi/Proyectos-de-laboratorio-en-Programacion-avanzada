24f79622650f40d65da100fa831a7c6559b91dac
Hugo <135773341+ExorcizedBoi@users.noreply.github.com>
2025-09-25 23:25:10 -0300

    Delete mi_carpeta directory

--- mi_carpeta/AFP.kt
+++
@@ -1,5 +0,0 @@
-class AFP(val nombre: String, val tasaDescuento: Double) {
-    override fun toString(): String {
-        return "$nombre (Tasa: ${tasaDescuento * 100}%)"
-    }
-}
--- mi_carpeta/Direccion.kt
+++
@@ -1,9 +0,0 @@
-class Direccion(
-    val calle: String,
-    val numero: String,
-    val ciudad: String
-) {
-    override fun toString(): String {
-        return "$calle $numero, $ciudad"
-    }
-}
